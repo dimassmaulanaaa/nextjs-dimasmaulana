4b956637f15223275f6f4c602d38df8030aa2b0f
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-09-11 20:33:56 +0700

    feat(config): allow images from vercel blob storage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL('https://sejdaw0pnkrbbizs.public.blob.vercel-storage.com/**')],
+  },
 };
 
 export default nextConfig;
