0e18ef64eda54b06e8c8e8094e60c87ff2e1aba5
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-09-19 22:29:47 +0700

    feat(util): create utility to format date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getFormatMonthYear(date: string) {
+  const options = {
+    year: "numeric",
+    month: "long",
+  } as const;
+
+  return new Date(date).toLocaleDateString("en-US", options);
 }
